870a676845c78b635ea9288aa45fb93ac1a6ee46
SojitraJenil <jenilsojitra4@gmail.com>
2024-07-13 19:22:01 +0530

    render

--- FrontEnd/src/API/Api.js
+++ FrontEnd/src/API/Api.js
@@ -1,3 +1,1 @@
-export const RenderHost =
-  "https://api.render.com/deploy/srv-cq97ogtds78s739a0or0?key=WJtFOPInf50";
-// export const RenderHost = "https://ecommerce-3-ul25.onrender.com";
+export const RenderHost = "https://ecommerce-3-ul25.onrender.com";
