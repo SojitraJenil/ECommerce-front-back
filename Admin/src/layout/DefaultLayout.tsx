db2698ca39d739e7b6ae2005182c6735aea6d71a
SojitraJenil <jenilsojitra4@gmail.com>
2024-09-18 13:08:00 +0530

    admin UI

--- Admin/src/layout/DefaultLayout.tsx
+++ Admin/src/layout/DefaultLayout.tsx
@@ -19,13 +19,11 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               {children}
             </div>
           </main>
-          {/* <!-- ===== Main Content End ===== --> */}
         </div>
         {/* <!-- ===== Content Area End ===== --> */}
       </div>
