6722d6f5c015640385a77d450e4b682fe1ae85e6
SojitraJenil <jenilsojitra4@gmail.com>
2024-09-18 13:18:27 +0530

    admin

--- Admin/src/layout/DefaultLayout.tsx
+++ Admin/src/layout/DefaultLayout.tsx
@@ -7,7 +7,6 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
 
   return (
     <div className="dark:bg-boxdark-2 dark:text-bodydark">
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
       <div className="flex h-screen overflow-hidden">
         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
